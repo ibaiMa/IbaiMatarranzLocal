147e0b30ebb845bc3c1370fe51351d39c683b3ee
Ibai <ikimatarranz24@lhusurbil.eus>
2025-02-25 10:20:53 +0100

    archivo modificado desde rama nueva

--- archivo3.java
+++ archivo3.java
@@ -1,9 +1,10 @@
-ublic class CuantasPalabras {
+public class CuantasPalabras {
     public static void main(String[] args) {
         int contador=0; 
         System.out.println("Introduce una oracion"); 
 
 
+			//este codigo esta mucho mejor
 
     }
 
